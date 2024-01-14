0654f5114e423cd9beb46fb4b07ba1f6dc887e6d
191203linhlinhkhanh <linhttkph35655@fpt.edu.vn>
2024-01-14 20:54:13 +0700

    Lab1

--- bai1/main.js
+++ bai1/main.js
@@ -1,18 +1,10 @@
 users.forEach(function (user) {
   var li = document.createElement("li");
-
-  // Tạo nội dung của thẻ li
   var genderText = user.gender === 1 ? "male" : "female";
   var liContent = document.createTextNode(
     user.id + " - " + user.name + " - " + genderText
   );
-
-  // Thêm nội dung vào thẻ li
   li.appendChild(liContent);
-
-  // Thêm lớp cho thẻ li tùy thuộc vào giới tính
   li.classList.add("user_item", user.gender === 1 ? "male" : "female");
-
-  // Thêm thẻ li vào phần tử có id là "target"
   target.appendChild(li);
 });
